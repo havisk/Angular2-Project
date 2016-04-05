ac63a8635643ec6db452a9acf85d9a5da07519f5
Kool Havis <havisk@gmail.com>
2016-04-04 22:45:34 -0400

    got show image button to work.  hover over image name appears, made image smaller

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -7,6 +7,9 @@ import { Component } from 'angular2/core';
 })
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
   products: any[] = [
     {
         "productId": 1,
@@ -30,4 +33,7 @@ export class ProductListComponent {
     }
   ];
 
+  toggleImage(): void  {
+    this.showImage = !this.showImage; 
+  }
 }
